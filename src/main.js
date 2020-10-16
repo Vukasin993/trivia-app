e143f6b49dd24f2bbdfb764f55155a4178e70eed
Vukasin Macura <moj_email@gmail.com>
2020-10-16 21:32:05 +0200

    2. zadatak: Install vuex, create 2 stores, create 2 services

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueRouter from 'vue-router'
 import router from './router'
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
